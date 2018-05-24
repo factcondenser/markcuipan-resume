dbc708c37d1716b05ae6b86c7b43cd595e0b652e
factcondenser <markpan@live.com>
2018-05-24 00:26:55 -0500

    Modify bash script helper

--- push-changes.sh
+++ push-changes.sh
@@ -4,6 +4,8 @@ YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+RESUME_NAME='markcuipan-resume'
+
 printf "${YELLOW}Enter a commit message:${NC} "
 read COMMIT_MESSAGE
 if [ "$COMMIT_MESSAGE" = "" ] ; then
@@ -11,16 +13,17 @@ if [ "$COMMIT_MESSAGE" = "" ] ; then
 fi
 
 echo -e "${GREEN}About to push changes to resume github repo${NC}" # -e to allow backslash escapes when using echo
-git add *-resume.pdf *-resume.log *-resume.xtx
+git add $RESUME_NAME.pdf $RESUME_NAME.log $RESUME_NAME.xtx
 git commit -m "$COMMIT_MESSAGE"
 git push origin master
 
-cp *-resume.pdf ~/workspace/markcuipan-dot-com/resume/
+cp $RESUME_NAME.pdf pdf_backups/$RESUME_NAME-$(date "+%Y-%m-%d-%H:%M:%S").pdf
+cp $RESUME_NAME.pdf ~/workspace/markcuipan-dot-com/resume/
 
 echo -e "${GREEN}Switching to portfolio site local repo${NC}"
 cd ~/workspace/markcuipan-dot-com/
 
 echo -e "${GREEN}About to push changes to portfolio site github repo${NC}"
-git add *markcuipan-resume.pdf
+git add $RESUME_NAME.pdf
 git commit -m "$COMMIT_MESSAGE"
 git push origin master
